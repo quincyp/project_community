716950c35f2ea65f459e51b0fe1aeb2a5613b411
qtpham <quincytpham@gmail.com>
2021-02-06 20:42:42 -0800

    Added edit route, edit route prefills resource detail data, returns to resource detail page to show update

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('resources/', views.resources_index, name='resources_index'),
-    path('resources/<int:resource_id>/detail', views.resources_detail, name='detail'),
+    path('resources/<int:resource_id>/detail', views.resources_detail, name='resources_detail'),
     path('resources/new', views.resources_new, name='resources_new'),
+    path('resources/<int:resource_id>/edit', views.resources_edit, name='resources_edit'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -13,10 +13,6 @@ def about(request):
     return render(request, 'about.html')
 
 def resources_index(request):
-    # REVIEW: POST request not working
-    print("================================")
-    print("here")
-    print("================================")
     if request.method == 'POST':
         resource_form = Resource_Form(request.POST)
         if resource_form.is_valid():
@@ -35,7 +31,21 @@ def about(request):
     return render(request, 'about.html')
 
 def resources_new(request):
-    # REVIEW: Form renders, POST action not working
     resource_form = Resource_Form()
     context = { 'resource_form': resource_form}
     return render(request, 'resources/new.html', context)
+
+def resources_edit(request, resource_id):
+    resource = Resource.objects.get(id=resource_id)
+    if request.method == 'POST':
+        resource_form = Resource_Form(request.POST, instance=resource)
+        if resource_form.is_valid():
+            # print("================================")
+            # print("here")
+            # print("================================")
+            resource_form.save()
+            return redirect('resources_detail', resource_id=resource.id)
+
+    resource_form = Resource_Form(instance=resource)
+    context = { 'resource': resource, 'resource_form': resource_form}
+    return render(request, 'resources/edit.html', context)
