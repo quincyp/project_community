746a9e65099f4e7d9fb4ab77a5dd86dab9fb777d
qtpham <quincytpham@gmail.com>
2021-02-08 15:37:54 -0800

    Maps now mostly working, markers show per resource address, user marker loaded with different symbol; also added quotes to edit resource data now showing full values not just first word of sentence

--- main_app/static/js/maps.js
+++ main_app/static/js/maps.js
@@ -1,53 +1,75 @@
 let geocoder;
 let map;
 
-function initMap() {
+// Creates the position lat/lng and passes to init map
+const success = (position) => {    
+    initMap({ 
+        lat: position.coords.latitude, 
+        lng: position.coords.longitude, 
+    });
+};
+const error = (error) => console.log(error);
+
+// Gets the current user location
+navigator.geolocation.getCurrentPosition(success, error);
+
+function initMap(position) {
     geocoder = new google.maps.Geocoder();
 
     // The location of Seattle
-    const seattle = { lat: 47.608, lng: -122.3321 };
+    const seattle = {
+        lat: 47.608,
+        lng: -122.3321
+    };
 
-    // The map, centered at Seattle
+    // The map, centered at user location
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 12,
-        center: seattle,
+        zoom: 13,
+        center: position,
     });
 
-    // The marker, positioned at Seattle
-    // const marker = new google.maps.Marker({
-    //     position: seattle,
-    //     map: map,
-    // });
-    codeAddress();
+    var circleSymbol = {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 8,
+        strokeColor: '#4285F4'
+    };
+
+    // The user's marker, set as circle to differentiate from other markers
+    const userMarker = new google.maps.Marker({
+        position: position,
+        icon: circleSymbol,
+        map: map,
+    });
+    userMarker.setAnimation(google.maps.Animation.DROP);
+
+    // Takes in address's and geocodes lat, lng then generates markers
+    codeAddress(map, geocoder);
 }
 
-
-function getMarkers() {
+function codeAddress(map, geocoder) {
     let address = document.querySelectorAll('.address');
-    for(let i = 0; i < address.length; i++) {
-        console.log(address[i]);
-        console.log(address[i].innerHTML);
+    console.log(address);
+
+    for (let i = 0; i < address.length; i++) {
+        geocoder.geocode({
+            'address': address[i].innerHTML
+        }, function (results, status) {
+            if (status == 'OK') {
+                // map.setCenter(results[0].geometry.location);
+                // const setPosition = { 
+                //     lat: results[0].geometry.location.lat(), 
+                //     lng: results[0].geometry.location.lng() 
+                // };
+                const marker = new google.maps.Marker({
+                    position: results[0].geometry.location,
+                    map: map,
+                });
+            } else {
+                alert('Geocode was not successful for the following reason: ' + status);
+            }
+        });
     }
 }
-
-
-function codeAddress() {
-    var address = document.querySelector('.address').innerHTML;
-    console.log(address);
-    geocoder.geocode( { 'address': address}, function(results, status) {
-        if (status == 'OK') {
-            // map.setCenter(results[0].geometry.location);
-            console.log(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                position: results[0].geometry.location,
-                map: map,
-            });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
-
 
 /*
 Test Locations:
