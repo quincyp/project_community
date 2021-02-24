487500b7cdf57a58b1e96853a19490a53c92a4ea
qtpham <quincytpham@gmail.com>
2021-02-24 11:00:09 -0800

    Added fa-hidden class for spacing, added bootstrap validation response

--- main_app/static/js/scripts.js
+++ main_app/static/js/scripts.js
@@ -1,3 +1,22 @@
+// Bootstrap validator, disabling form submissions if there are invalid fields
+(() => {
+    'use strict';
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach((form) => {
+        form.addEventListener('submit', (event) => {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+        }, false);
+    });
+})();
+
 // Keep sizing dropdown menu open
 $('.dropdown-stay').on('click', function (e) {
     console.log('propogate');
